99cd61e34879c348ae4bd28e71bcf01ee5f5ab02
mgd1984 <alecjohnmiller@gmail.com>
2023-05-09 16:13:28 -0400

    removed &dl=0

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -27,7 +27,7 @@ const Live = () => (
         </div>
       </header>
       <p>
-        <iframe title="Kepler Plugin" src="https://www.dropbox.com/s/jn2nsm193t6inwx/kepler.gl%20%281%29.json?dl=0" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" height="600px" width="800px" allowFullScreen />
+        <iframe title="Kepler Plugin" src="https://www.dropbox.com/s/jn2nsm193t6inwx/kepler.gl%20%281%29.json" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" height="600px" width="800px" allowFullScreen />
       </p>
       {/* <ReactMarkdown
         source={markdown}
