30300f747db8d8194f82d2ff9677f5ff39a20a52
mgd1984 <alecjohnmiller@gmail.com>
2023-05-09 16:07:51 -0400

    updated kepler map with telco data

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -27,7 +27,7 @@ const Live = () => (
         </div>
       </header>
       <p>
-        <iframe title="Kepler Plugin" src="https://kepler.gl/#/demo?mapUrl=https://raw.githubusercontent.com/ikespand/ikespand.github.io/master/_data/sample_data/keplergl_road_network.json" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" height="600px" width="800px" allowFullScreen />
+        <iframe title="Kepler Plugin" src="https://kepler.gl/#/demo?mapUrl=https://drive.google.com/file/d/1A5cvvTTu_-vN-QYvK4hME4NUNmkvgSbY/view?usp=sharing" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" height="600px" width="800px" allowFullScreen />
       </p>
       {/* <ReactMarkdown
         source={markdown}
