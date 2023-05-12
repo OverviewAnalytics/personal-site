7b610de79e59718187a30fcfa028874960f53127
mgd1984 <alecjohnmiller@gmail.com>
2023-05-11 21:34:56 -0400

    changed contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -23,7 +23,6 @@ const Contact = () => (
         <p>Contact Us: </p>
         <EmailLink />
         <iframe title="intake-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeab0vTCsMLmxKCb-kNwu46QlcqGaBIqZYPR_V_94wKOeOUJQ/viewform?embedded=true" width="800" height="800%" frameBorder="0" marginhHeight="0" marginWidth="0" />
-      </header>
       </div>
       {/* <header>
         <div className="title">
