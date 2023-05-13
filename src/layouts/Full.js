287b9f9a98980ca4b81779a223aa87a477cc0b27
mgd1984 <alecjohnmiller@gmail.com>
2023-05-13 14:46:45 -0400

    fullPage edtis

--- src/layouts/Full.js
+++ src/layouts/Full.js
@@ -20,7 +20,7 @@ const Full = (props) => (
       <div id="full">
         {props.children}
       </div>
-      {props.fullPage ? true : <SideBar />}
+      {props.fullPage}
     </div>
   </HelmetProvider>
 );
