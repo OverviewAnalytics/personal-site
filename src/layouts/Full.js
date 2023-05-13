91f11dd1a6c766aca5fc1a3c7ff33feb22a16e5a
mgd1984 <alecjohnmiller@gmail.com>
2023-05-13 15:49:51 -0400

    uncommented sidebar import

--- src/layouts/Full.js
+++ src/layouts/Full.js
@@ -4,7 +4,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import Analytics from '../components/Template/Analytics';
 import Navigation from '../components/Template/Navigation';
-// import SideBar from '../components/Template/SideBar';
+import SideBar from '../components/Template/SideBar';
 import ScrollToTop from '../components/Template/ScrollToTop';
 
 const Full = (props) => (
