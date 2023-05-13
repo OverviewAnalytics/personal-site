0a552231ad1fba9572a3039c836b3d4bc0905297
mgd1984 <alecjohnmiller@gmail.com>
2023-05-13 12:23:38 -0400

    changed maps page layout

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 // import ReactMarkdown from 'react-markdown';
 // import raw from 'raw.macro';
 
-import Main from '../layouts/Main';
+// import Main from '../layouts/Main';
 
 // uses babel to load contents of file
 // const markdown = raw('../data/about.md');
