a340c00a603925791e5e59414bf4e649e6578931
mgd1984 <alecjohnmiller@gmail.com>
2023-05-13 15:47:27 -0400

    made changes to full.js and main.js

--- src/layouts/Full.js
+++ src/layouts/Full.js
@@ -20,8 +20,8 @@ const Full = (props) => (
       <div id="full">
         {props.children}
       </div>
-      {props.fullPage}
-    </div>
+      {!props.fullPage && <SideBar />}
+      </div>
   </HelmetProvider>
 );
 
