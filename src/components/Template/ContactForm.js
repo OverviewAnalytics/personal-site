d4858987e6e5f42ea8bcbbb82392229d93c6f85a
mgd1984 <alecjohnmiller@gmail.com>
2023-05-13 23:33:40 -0400

    edited contact form syntax

--- src/components/Template/ContactForm.js
+++ src/components/Template/ContactForm.js
@@ -3,16 +3,18 @@ import { useForm } from 'react-hook-form';
 
 const Form = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  // eslint-disable-next-line no-console
+  const onSubmit = (data) => console.log(data);
+  // eslint-disable-next-line no-console
   console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
-      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
-      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
-      <select {...register("Title", { required: true })}>
+      <input type="text" placeholder="First name" {...register('First name', { required: true, maxLength: 80 })} />
+      <input type="text" placeholder="Last name" {...register('Last name', { required: true, maxLength: 100 })} />
+      <input type="text" placeholder="Email" {...register('Email', { required: true, pattern: /^\S+@\S+$/i })} />
+      <input type="tel" placeholder="Mobile number" {...register('Mobile number', { required: true, minLength: 6, maxLength: 12 })} />
+      <select {...register('Title', { required: true })}>
         <option value="Mr">Mr</option>
         <option value="Mrs">Mrs</option>
         <option value="Miss">Miss</option>
