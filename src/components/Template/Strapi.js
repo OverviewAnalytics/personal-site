38f091360e0286e907b7fb791d674591c0c5bf64
mgd1984 <alecjohnmiller@gmail.com>
2023-05-15 12:55:01 -0400

    edited strapi component

--- src/components/Template/Strapi.js
+++ src/components/Template/Strapi.js
@@ -16,17 +16,6 @@ const Strapi = () => {
 
     fetchData();
   }, []);
-
-  return (
-    <div>
-      {data.map(item => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <p>{item.description}</p>
-        </div>
-      ))}
-    </div>
-  );
 };
 
 export default Strapi;
