cb9608c8ea2b0917e07a03603dee8f419a1a907b
mgd1984 <alecjohnmiller@gmail.com>
2023-05-17 16:31:06 -0400

    edited live.js

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import ReactMarkdown from 'react-markdown';
 // import raw from 'raw.macro';
 
