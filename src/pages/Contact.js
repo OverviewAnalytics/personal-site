4ee97f57669c33ccffa93d41caca6a42a6c4cadd
mgd1984 <alecjohnmiller@gmail.com>
2023-05-17 13:10:07 -0400

    changed contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -27,17 +27,40 @@ const Contact = () => (
           <iframe title="intake-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeab0vTCsMLmxKCb-kNwu46QlcqGaBIqZYPR_V_94wKOeOUJQ/viewform?embedded=true" width="800" height="1000" />
         </p> */}
       </div>
-      <div className="contact-form">
+      {/* <div className="contact-form">
         <p>
           <Form />
         </p>
-      </div>
+      </div> */}
+      
       {/* <header>
         <div className="title">
           <iframe title="square-booking-flow" src="https://square.site/appointments/buyer/widget/9x0957pgxktr2f/LNBV7SAYVA2FZ" width="100%" height="1300" frameBorder="0" scrolling="yes" />
         </div>
       </header> */}
       {/* <ContactIcons /> */}
+    </article>
+    <article className="post markdown" id="team">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/team">team</Link></h2>
+        </div>
+      </header>
+      {/* <ReactMarkdown
+        source={markdown}
+        renderers={{
+          Link: LinkRenderer,
+        }}
+        escapeHtml={false}
+      /> */}
+      <p>Overview Analytics was founded by <a className="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/alecjmiller?trk=profile-badge"> Alec Miller </a>
+        who holds an <a href="https://ivey.uwo.ca/"> Honours Business Administration degree</a> from the Ivey School of Business and a Certificate in Data Science from <a href="https://learn.utoronto.ca/programs-courses/certificates/data-science">University of Toronto SCS. </a>
+        Prior to <a href="https://www.overviewanalytics.ca">Overview Analytics</a>, Alec worked in technology,
+        telecom, and startup incubation including roles @ <a href="https://rogers.com">Rogers</a>
+        , <a href="https://marsdd.com">MaRS</a>
+        , <a href="https://techalliance.ca">TechAlliance</a>
+        , and <a href="https://lenovo.com">Lenovo</a>.
+      </p>
     </article>
   </Main>
 );
