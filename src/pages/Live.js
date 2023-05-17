5e7c425d68825b032c77d769fa7780fac5a4a886
mgd1984 <alecjohnmiller@gmail.com>
2023-05-17 16:29:23 -0400

    edited live.js layout for more screen RE

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -21,19 +21,19 @@ const Live = () => (
     description="Livestreaming Video"
   >
     <article className="post markdown" id="live">
-      <header>
+      {/* <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/live">live</Link></h2>
         </div>
-      </header>
+      </header> */}
       <p>
-        The map below is a demo app built using the Kepler.gl GIS visualization library.
+        <iframe title="Kepler Plugin" src="https://kepler.gl/#/demo?mapUrl=https://raw.githubusercontent.com/OverviewAnalytics/KeplerDemo/main/kepler.gl%20(5).json" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" height="650" width="100%" allowFullScreen />
+      </p>
+      <p>
+        The map above is a demo app built using the Kepler.gl GIS visualization library.
         It takes location data from Industry Canada Spectrum Management System
         Database and maps it using the incredible in-browser,
         hardware-accelerated capabilities of Kepler.gl.
-      </p>
-      <p>
-        <iframe title="Kepler Plugin" src="https://kepler.gl/#/demo?mapUrl=https://raw.githubusercontent.com/OverviewAnalytics/KeplerDemo/main/kepler.gl%20(5).json" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" height="650" width="100%" allowFullScreen />
       </p>
       {/* <ReactMarkdown
         source={markdown}
